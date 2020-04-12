d94a12563322afab538248a21607ebff2ad64d75
dhythm <Yuta.Okada.20@gmail.com>
2020-04-12 11:39:29 +0900

    update

--- src/app/App.js
+++ src/app/App.js
@@ -1,42 +1,14 @@
-import Box from 'core/components/atoms/Box';
-import Loading from 'core/components/atoms/Loading';
-import React, { useEffect, useState } from 'react';
-import { Dimensions, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import PicturePage from './pages/PicturePage';
 
 const App = () => {
-  const [imageUrl, setImageUrl] = useState(null);
-  useEffect(() => {
-    fetch('https://source.unsplash.com/random').then((response) => {
-      setImageUrl(response?.url);
-    });
-  }, []);
-
-  if (!imageUrl) {
-    return (
-      <>
-        <Loading />
-      </>
-    );
-  }
-
   return (
-    <Box flex={1} justifyContent="center" alignItems="center">
-      <Image
-        style={styles.img}
-        source={{ uri: imageUrl }}
-        resizeMode="contain"
-      />
-    </Box>
+    <NavigationContainer>
+      <PicturePage />
+    </NavigationContainer>
   );
 };
-
-const { width, height } = Dimensions.get('screen');
-
-const styles = StyleSheet.create({
-  img: {
-    width,
-    height,
-  },
-});
 
 export default App;
